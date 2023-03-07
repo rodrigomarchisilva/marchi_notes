b91f5e1b542e5b3eab15ee358bccf34af3b4b3a8
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2023-03-07 16:51:30 -0300

    :sparkles: feat(new): add simple array sum challenge in hackerrank section

---
+++ challenges/hackerrank/simple_array_sum.js
@@ -0,0 +1,9 @@
+// Given an array of integers, find the sum of its elements.
+
+const simpleArraySum = (ar) => ar.reduce((a, b) => a + b);
+
+console.log(simpleArraySum([1, 2, 3, 4, 10, 11]));
+
+// Expected output: 31
+
+// https://www.hackerrank.com/challenges/simple-array-sum/problem
